8dd267ddfcdd8e0f9053fd81180ec30ad505dcad
GreyZeng <410486047@qq.com>
2022-07-02 15:42:31 +0800

    func

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+int power(int m, int n);
 main()
 {
 	printf("hello world\n");
-	word_count();
+	printf("4^2 = %d", power(4, 2));
+}
+int power(int m, int n)
+{
+	int i, p;
+	p = 1;
+	for (i = 1; i <= n; ++i)
+	{
+		p = p * m;
+	}
+	return p;
 }
